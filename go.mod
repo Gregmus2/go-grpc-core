f1a6e8f74b861edc4058880af97102ddbd05b11f
gregmus <gregmus4@gmail.com>
2024-02-18 08:43:06 +0100

    add auth interceptor

--- go.mod
+++ go.mod
@@ -4,23 +4,25 @@ go 1.21
 
 require (
 	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/google/uuid v1.6.0
+	github.com/gregmus2/poll-play-proto-gen v0.0.0-20240218074037-72f628f30af5
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.1
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/takt-corp/fx-logrus v1.0.1
 	go.uber.org/fx v1.20.1
-	google.golang.org/grpc v1.61.0
+	google.golang.org/grpc v1.61.1
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/dig v1.17.1 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240125205218-1f4bbc51befe // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240213162025-012b6fc9bca9 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )
---
+++ interceptors/auth.go
@@ -0,0 +1,82 @@
+package interceptors
+
+import (
+	"context"
+	"github.com/caarlos0/env"
+	"github.com/google/uuid"
+	"github.com/gregmus2/poll-play-proto-gen/go/private"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/auth"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/metadata"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+const authorizationHeaderName = "authorization"
+const ContextUserIdKey = "user-id"
+const AuthorizationInterceptorName = "AuthorizationInterceptor"
+
+type authenticationConfig struct {
+	Address string `env:"USER_SERVICE_ADDRESS" envDefault:"user:9001"`
+}
+
+type AuthorizationInterceptor struct {
+	userClient private.UserServiceClient
+}
+
+func (i *AuthorizationInterceptor) Init() error {
+	cfg := new(authenticationConfig)
+
+	if err := env.Parse(cfg); err != nil {
+		return errors.Wrap(err, "error parsing required ENV config for initializing authorization interceptor")
+	}
+
+	cc, err := grpc.Dial(cfg.Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return errors.Wrap(err, "error creating connection with authorization service")
+	}
+
+	i.userClient = private.NewUserServiceClient(cc)
+
+	return nil
+}
+
+func (i *AuthorizationInterceptor) UnaryInterceptor() grpc.UnaryServerInterceptor {
+	return auth.UnaryServerInterceptor(i.auth)
+}
+
+func (i *AuthorizationInterceptor) StreamInterceptor() grpc.StreamServerInterceptor {
+	return auth.StreamServerInterceptor(i.auth)
+}
+
+func (i *AuthorizationInterceptor) Name() string {
+	return AuthorizationInterceptorName
+}
+
+func (i *AuthorizationInterceptor) DependsOn() []string {
+	return []string{}
+}
+
+func (i *AuthorizationInterceptor) auth(ctx context.Context) (context.Context, error) {
+	token := metadata.ExtractIncoming(ctx).Get(authorizationHeaderName)
+	if token == "" {
+		return nil, status.Error(codes.Unauthenticated, "authorization token is required")
+	}
+
+	resp, err := i.userClient.Authenticate(context.Background(), &private.AuthenticateRequest{
+		Token: token,
+	})
+
+	if err != nil {
+		// auth service returns errors with code, so no need to wrap
+		return nil, err
+	}
+
+	ctx = logging.InjectFields(ctx, logging.Fields{"grpc.request.id", uuid.New().String()})
+	ctx = logging.InjectFields(ctx, logging.Fields{"user.user_id", resp.User.Id})
+
+	return context.WithValue(ctx, ContextUserIdKey, resp.User.Id), nil
+}
